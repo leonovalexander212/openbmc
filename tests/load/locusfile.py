71a68980988c2a59d5f8c5caf8b89e72aa00f77d
leonovalexander212 <124764994+leonovalexander212@users.noreply.github.com>
2025-05-19 22:56:15 +0700

    Update locusfile.py

--- tests/load/locusfile.py
+++ tests/load/locusfile.py
@@ -1,8 +1,25 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, between, task
 
-class BMCUser(HttpUser):
-    wait_time = between(1, 3)
+class OpenBMC(HttpUser):
+    wait_time = between(1,5)
+    host = "https://localhost:2443/redfish/v1"
 
     @task
-    def get_sensors(self):
-        self.client.get("/redfish/v1/Chassis", verify=False)
+    def auth_open_bmc(self):
+        self.client.get("/SessionService/Sessions", auth=("root", "0penBmc"), verify=False)
+
+    @task
+    def info_open_bmc(self):
+        self.client.get("/Systems/system", auth=("root", "0penBmc"), verify=False)
+
+class PublicAPI(HttpUser):
+    wait_time = between(1,2)
+    host = "https://jsonplaceholder.typicode.com"
+
+    @task
+    def jsonplaceholder(self):
+        self.client.get("/posts")
+
+    @task
+    def weather(self):
+        self.client.get("https://wttr.in/Novosibirsk?format=j1", verify=False)
